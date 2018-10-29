16deaa48ff468b4b511f4c087a7b29f360a5a62f
github username <chao.alexander.123@gmail.com>
2018-10-29 19:17:49 -0400

    define routes to output to the response body

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    resp = Rack::Response.new
+    resp.write "My name is Alex"
+  end
+
+  get '/hometown' do
+    resp = Rack::Response.new
+    resp.write "My hometown is Great Neck"
+  end
+
+  get '/favorite-song' do
+    resp = Rack::Response.new
+    resp.write "My favorite song is 'Follow Me'"
+  end
+
 end
